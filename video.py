9df917ef7a14a4f73611fb80fc36e5046d599bb3
gitrust <weebac@web.de>
2018-01-03 20:58:45 +0100

    Additional video processing functions

--- video.py
+++ video.py
@@ -2,6 +2,8 @@ from moviepy.editor import *
 import sys
 import datetime
 import os
+import moviepy.video.fx.all as vfx
+import scipy.ndimage as ndimage
 
 # Combines video files in a given directory to one file with an additional title
 #
@@ -14,9 +16,9 @@ vlist   = "video.list"
 # The range of the CRF scale is 0–51, where 0 is lossless, 23 is the default, and 51 is worst quality possible.
 # A lower value generally leads to higher quality, and a subjectively sane range is 17–28. 
 # Consider 17 or 18 to be visually lossless or nearly so
-videoquality = "23"
+videoquality = "50"
 # slow, ultrafast, superfast, veryfast, faster, fast, medium, slow, slower, veryslow
-compression = "slow"
+compression = "ultrafast"
 # included extensions of files which will be processed
 extensions = ["mp4","avi"]
 # max characters: 18 
@@ -32,17 +34,16 @@ def listfiles(path):
 				f.append(filename)
 	return f
 
-def addtext(clips,text):
-	# append textclip to all clips at first position
+# add centered title at the beginning of the clip
+def addtext(clip,text):
 	# Generate a text clip. You can customize the font, color, etc.
 	# set textclip size from the first video clip
 	txt_clip = TextClip(text,
 			fontsize=txtfontsize,
 			font="Arial",
 			color='white',
-			size=clips[0].size,
 			transparent=True).set_pos('center').set_duration(4)
-	clips[0] = CompositeVideoClip([clips[0], txt_clip])
+	return CompositeVideoClip([clip, txt_clip])
 	
 def files2clips(files):
 	clips=[]
@@ -53,6 +54,38 @@ def files2clips(files):
 		print("Add file to clips " + fname)
 		clips.append(VideoFileClip(fname,audio_buffersize=15000))
 	return clips
+
+# blur image
+def blur(img):
+	return ndimage.gaussian_filter(img, sigma=(2, 2, 0), order=0)
+
+def files2clips(files):
+	clips=[]
+	for fname in files:
+		# skip comments
+		if fname.startswith("#"):
+			continue
+		print("Add file to clips " + fname)
+		clips.append(VideoFileClip(fname,audio_buffersize=15000))
+	return clips
+	
+def crossfade(clips):
+
+	# cross fade each clip
+	fade_duration = 1 # 1-second 
+	clips = [clip.crossfadein(fade_duration) for clip in clips]
+
+	# concat clips
+	# The "padding" ensures that the clips will overlap for 1 second
+	return  concatenate(clips, padding = -fade_duration,method="compose")
+
+def blacknwhite(clip):
+	return clip.fx(vfx.blackwhite)
+
+def write(clip,tofile):
+		# Write the result to a file
+	print("Final clip length is " + str(datetime.timedelta(seconds=clip.end)))
+	clip.write_videofile(tofile,fps=24, codec=vcodec,preset=compression,ffmpeg_params=["-crf",videoquality])
 	
 def convert(clips,tofile):
 
@@ -69,7 +102,14 @@ def convert(clips,tofile):
 	# Write the result to a file
 	print("Final clip length is " + str(datetime.timedelta(seconds=final_clip.end)))
 	final_clip.write_videofile(tofile,fps=24, codec=vcodec,preset=compression,ffmpeg_params=["-crf",videoquality])
-
+	
+def withaudio(clip,audiofile):
+	audio = AudioFileClip(audiofile)
+	clip.set_audio(audio)
+	
+def blurclip(clip):
+	return clip.fl_image(blur)
+	
 def main(argv):
 	if len(argv) < 2:
 		print("Usage: video.py path text")
@@ -77,11 +117,17 @@ def main(argv):
 		
 	path = argv[0]
 	text = argv[1]
+	
 	files = listfiles(path)
 	
 	clips = files2clips(files)
-	addtext(clips,text)
-	convert(clips,"final.mp4")
+	clip = crossfade(clips)
+	clip = blacknwhite(clip)
+	clip = blurclip(clip)
+	clip = clip.fadeout(3)
+	clip = addtext(clip,text)
+	write(clip,"final.mp4")
+	
 	
 if __name__=='__main__':
     main(sys.argv[1:])
