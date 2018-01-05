61a7fe25ba593bf3ceac0429a43ee9e0c8f1da89
gitrust <weebac@web.de>
2018-01-05 22:38:33 +0100

    Add code to rotate cellphone videos, clean up

--- video.py
+++ video.py
@@ -16,11 +16,11 @@ vlist   = "video.list"
 # The range of the CRF scale is 0–51, where 0 is lossless, 23 is the default, and 51 is worst quality possible.
 # A lower value generally leads to higher quality, and a subjectively sane range is 17–28. 
 # Consider 17 or 18 to be visually lossless or nearly so
-videoquality = "50"
+videoquality = "24"
 # slow, ultrafast, superfast, veryfast, faster, fast, medium, slow, slower, veryslow
-compression = "ultrafast"
+compression = "slow"
 # included extensions of files which will be processed
-extensions = ["mp4","avi"]
+extensions = ["mp4","avi","mov"]
 # max characters: 18 
 txtfontsize = 60
 
@@ -31,7 +31,7 @@ def listfiles(path):
 		for filename in sorted(files):
 			# filter only movie files
 			if any(filename.lower().endswith(x) for x in extensions):
-				f.append(filename)
+				f.append(filename)				
 	return f
 
 # add centered title at the beginning of the clip
@@ -56,21 +56,14 @@ def files2clips(files):
 	return clips
 
 # blur image
-def blur(img):
-	return ndimage.gaussian_filter(img, sigma=(2, 2, 0), order=0)
+# sigma=5 - very blurred
+def blur(img,sigma=3):
+	
+	return ndimage.gaussian_filter(img, sigma)
 
-def files2clips(files):
-	clips=[]
-	for fname in files:
-		# skip comments
-		if fname.startswith("#"):
-			continue
-		print("Add file to clips " + fname)
-		clips.append(VideoFileClip(fname,audio_buffersize=15000))
-	return clips
 	
 def crossfade(clips):
-
+	
 	# cross fade each clip
 	fade_duration = 1 # 1-second 
 	clips = [clip.crossfadein(fade_duration) for clip in clips]
@@ -79,29 +72,22 @@ def crossfade(clips):
 	# The "padding" ensures that the clips will overlap for 1 second
 	return  concatenate(clips, padding = -fade_duration,method="compose")
 
+# black and white video
 def blacknwhite(clip):
 	return clip.fx(vfx.blackwhite)
 
 def write(clip,tofile):
 		# Write the result to a file
 	print("Final clip length is " + str(datetime.timedelta(seconds=clip.end)))
-	clip.write_videofile(tofile,fps=24, codec=vcodec,preset=compression,ffmpeg_params=["-crf",videoquality])
-	
-def convert(clips,tofile):
+	clip.write_videofile(tofile,threads=4,fps=24, codec=vcodec,preset=compression,ffmpeg_params=["-crf",videoquality])
 
-	# cross fade each clip
-	fade_duration = 1 # 1-second 
-	clips = [clip.crossfadein(fade_duration) for clip in clips]
-
-
-	# concat clips
-	# The "padding" ensures that the clips will overlap for 1 second
-	final_clip = concatenate(clips, padding = -fade_duration,method="compose").fadeout(3)
-
-
-	# Write the result to a file
-	print("Final clip length is " + str(datetime.timedelta(seconds=final_clip.end)))
-	final_clip.write_videofile(tofile,fps=24, codec=vcodec,preset=compression,ffmpeg_params=["-crf",videoquality])
+# zoom fx
+# clip.fx(zoom,factor=1)
+def zoom(clip, factor):
+    c = (1-1.0/factor) / 2
+    x_crop = int(c * clip.w)
+    y_crop = int(c * clip.h)
+    return clip.crop(x_crop, y_crop, -x_crop, -y_crop).resize(clip.size)
 	
 def withaudio(clip,audiofile):
 	audio = AudioFileClip(audiofile)
@@ -109,23 +95,42 @@ def withaudio(clip,audiofile):
 	
 def blurclip(clip):
 	return clip.fl_image(blur)
+
+# rotate a cellphone video
+#
+# add blurred background video
+# Info: using angle of 90 causes an error
+# Consider aspect ratio of original videos
+def cellphone_rotate(clip,newsize,angle):
+	# original clip, rotated
+	w,h = moviesize = clip.size
+	movie = clip.fx(vfx.rotate,angle,expand=True).resize(height=newsize[1])
+
+	# background clip, rotated and stretched, blurred, zoom in
+	bg = movie.resize(newsize).without_audio().fl_image(blur).fx(zoom,2)
+
+	# a compose of both clips
+	# position original movie at center
+	return CompositeVideoClip([bg,movie.set_pos("center")]).set_duration(movie.duration)
+
 	
 def main(argv):
 	if len(argv) < 2:
-		print("Usage: video.py path text")
+		print("Usage: video.py path-to-videos text")
 		sys.exit(0)
 		
 	path = argv[0]
 	text = argv[1]
 	
 	files = listfiles(path)
-	
 	clips = files2clips(files)
 	clip = crossfade(clips)
-	clip = blacknwhite(clip)
-	clip = blurclip(clip)
-	clip = clip.fadeout(3)
+	#clip = blacknwhite(clip)
+	#clip = blurclip(clip)
+	
+	clip = cellphone_rotate(clip,(1024,768),-89.99999999)
 	clip = addtext(clip,text)
+	clip = clip.fadeout(3)
 	write(clip,"final.mp4")
 	
 	
